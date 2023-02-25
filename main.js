deedc155b180b208d64f896912f2cc5325991712
SuperCoolGamer <103328500+SuperCoolGamer@users.noreply.github.com>
2023-02-24 18:59:47 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseY = 0;
+noseX = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500)
@@ -11,14 +17,27 @@ function setup(){
 
 function draw(){
     background('#969A97');
+    textSize(difference)
+    fill("#F90093")
+    text("Hi there", noseX, noseY)
+    floor(noseX, noseY -70, difference)
 }
 
 function modelLoaded(){
-    console.assert.log('PoseNet Is Initialized!');
+    console.log('PoseNet Is Initialized!');
 }
 
 function gotPoses(results){
     if(results.length > 0){
-console.log(results);
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX =results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWrist = " + leftWristX + "rightWristX = "+ rightWristX + " difference = " + difference);
     }
 }
